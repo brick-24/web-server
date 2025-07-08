23574c5d612daba096bf1bd65c41923c73a185a5
COMMANDO2406 <r@gmail.com>
2025-07-08 15:49:27 +0000

    Helper tool

---
+++ listen.py
@@ -0,0 +1,18 @@
+import requests
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="enter file name")
+args = parser.parse_args()
+
+
+res = requests.get(f"http://localhost:8080/{args.filename}")
+
+# Print the status, headers, and body
+print("Status code:", res.status_code)
+print("\nHeaders:")
+for key, value in res.headers.items():
+    print(f"{key}: {value}")
+
+print("\nBody:")
+print(res.text)
